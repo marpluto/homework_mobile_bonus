43a2265124f185c1deef36dbb372d5653264cd72
Pluto <maryonpluto@yandex.ru>
2023-03-22 18:02:07 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int balance = 100;
+        int payment = 1200;
+        int newBalance;
+        int bonus;
+        if (payment > 1000) {
+            bonus = payment / 100;
+        } else {
+            bonus = 0;
+        }
+        newBalance = balance + payment + bonus;
+        System.out.println("Текущий баланс: " + newBalance);
+        System.out.println("Количетсво бонусов: " + bonus);
+    }
+}
